61dea2c69e18c9c08da3dcd288b78c7fa47ee156
ndelanhese <nnathanh3@gmail.com>
2023-08-16 16:41:46 -0300

    feat(images) 🖼️  added quality on image compress

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-// const Compressor = require('compressorjs');
-
-const imageCompressor = async (file, width, height) => {
+const imageCompressor = async (file, width, height, quality) => {
     new Compressor(file, {
-        quality: 0.7,
+        quality: Number(quality),
         width: width,
-        maxWidth: 1920,
         height: height,
-        maxHeight: 1080,
         success(result) {
             console.log('Original image size:', file.size);
             console.log('Compressed image size:', result.size);
@@ -26,6 +22,38 @@ const imageCompressor = async (file, width, height) => {
     });
 }
 
+const videoCompressor = async (file, width, height, hasAudio) => {
+    // console.log('Compressing video...');
+    // const ffmpeg = createFFmpeg({
+    //     log: true,
+    // });
+
+    // ffmpeg.load();
+
+    // ffmpeg.FS('writeFile', 'input.mp4', await fetchFile(file));
+
+    // ffmpeg.run('-i', 'input.mp4', '-vf', `scale=${width}:${height}`, '-c:a', 'copy', '-c:v', 'libx264', '-crf', '18', 'output.mp4');
+
+    // ffmpeg.FS('readFile', 'output.mp4');
+
+    // const data = ffmpeg.FS('readFile', 'output.mp4');
+
+    // const blob = new Blob([data.buffer], {
+    //     type: 'video/mp4',
+    // });
+
+    // const url = URL.createObjectURL(blob);
+
+    // const a = document.createElement('a');
+    // a.href = url;
+    // a.download = 'compressed.mp4';
+    // a.click();
+
+    // URL.revokeObjectURL(url);
+
+    // console.log('Compressed video:', url);
+    // return (url)
+}
 
 
 const convertHeifToJpg = async (heifFile) => {
@@ -45,8 +73,9 @@ const convertHeifToJpg = async (heifFile) => {
 const compressor = async () => {
     const inputFile = document.getElementById('file');
     const loadingLabel = document.getElementById('loading');
-    const width = document.getElementById('width')?.value || 400;
-    const height = document.getElementById('height')?.value || 400;
+    const width = document.getElementById('width')?.value;
+    const height = document.getElementById('height')?.value;
+    const quality = document.getElementById('quality')?.value ?? 0.7;
     const hasAudio = document.getElementById('hasAudio')?.target?.value;
 
     const file = inputFile.files[0]
@@ -60,12 +89,12 @@ const compressor = async () => {
 
         if (imageType.includes('heif')) {
             const jpgFile = await convertHeifToJpg(file);
-            await imageCompressor(jpgFile, width, height);
+            await imageCompressor(jpgFile, width, height, quality);
         }
 
         if (!imageType.includes('heif')) {
             console.log(file)
-            await imageCompressor(file, width, height)
+            await imageCompressor(file, width, height, quality)
         }
     }
 
