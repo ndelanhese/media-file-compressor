e9a3fa0db07e3995d1c6111f7f5881704e227333
ndelanhese <nnathanh3@gmail.com>
2023-08-17 15:13:59 -0300

    feat(video) 🎥 finished video compress

--- index.js
+++ index.js
@@ -1,127 +1,126 @@
+// import { FFmpeg } from "@ffmpeg/ffmpeg";
+// import { fetchFile, toBlobURL } from "@ffmpeg/util";
+// import Compressor from "compressorjs";
+// import heic2any from "heic2any";
+
 const imageCompressor = async (file, width, height, quality) => {
-    new Compressor(file, {
-        quality: Number(quality),
-        width: width,
-        height: height,
-        success(result) {
-            console.log('Original image size:', file.size);
-            console.log('Compressed image size:', result.size);
-            const url = URL.createObjectURL(result);
-
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = file.name;
-            a.click();
-
-            URL.revokeObjectURL(url);
-            return (result)
-        },
-        error(err) {
-            console.error('Compression error:', err);
-        },
-    });
-}
-
-const videoCompressor = async (file, width, height, hasAudio) => {
-    console.log('compressing video...');
-    const {
-        createFFmpeg
-    } = FFmpeg;
-    const ffmpeg = await createFFmpeg({
-        log: true,
-    });
-    await ffmpeg.load();
-    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile(file));
-
-    const ffmpegArgs = ['-i', 'input.mp4', '-vf', `scale=${width}:${height}`, '-c:v', 'libx264', '-crf', '18'];
-
-    if (!hasAudio) {
-        ffmpegArgs.push('-an');
-    } else {
-        ffmpegArgs.push('-c:a', 'copy');
-    }
-
-    ffmpegArgs.push('output.mp4');
-
     try {
-        await ffmpeg.run(...ffmpegArgs);
-    } catch (err) {
-        console.error(err);
+        new Compressor(file, {
+            quality: Number(quality),
+            width,
+            height,
+            success(result) {
+                console.log('Original image size:', file.size);
+                console.log('Compressed image size:', result.size);
+                const url = URL.createObjectURL(result);
+                downloadFile(url, file.name);
+            },
+            error(err) {
+                console.error("Compression error:", err);
+            },
+        });
+    } catch (error) {
+        console.error("Image compression error:", error);
     }
+};
 
-    ffmpeg.FS('readFile', 'output.mp4');
+const videoCompressor = async (file, width, height, hasAudio, quality) => {
+    try {
+        const ffmpeg = new FFmpeg();
+        const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.2/dist/umd";
 
-    const data = ffmpeg.FS('readFile', 'output.mp4');
+        ffmpeg.on("progress", ({ progress }) => {
+            const roundedProgress = Math.round(progress * 100);
+            console.log(`${roundedProgress}%`);
+        });
 
-    const blob = new Blob([data.buffer], {
-        type: 'video/mp4',
-    });
+        await ffmpeg.load({
+            coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+            wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
+        });
 
-    const url = URL.createObjectURL(blob);
+        await ffmpeg.writeFile("input.mp4", await fetchFile(file));
 
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'compressed.mp4';
-    a.click();
+        const ffmpegArgs = [
+            "-i", "input.mp4",
+            "-vf", `scale=${width}:${height}`,
+            "-c:v", "libx264", "-crf", "18",
+            "-b:v", `${quality}M`,
+            ...(hasAudio ? ["-c:a", "copy"] : ["-an"]),
+            "output.mp4",
+        ];
 
-    URL.revokeObjectURL(url);
+        try {
+            await ffmpeg.exec(ffmpegArgs);
+        } catch (err) {
+            console.error("FFmpeg execution error:", err);
+        }
 
-    console.log('Compressed video:', url);
-    return url;
+        const data = await ffmpeg.readFile("output.mp4");
+
+        const blob = new Blob([data.buffer], {
+            type: "video/mp4",
+        });
+
+        downloadFile(URL.createObjectURL(blob), "compressed.mp4");
+    } catch (error) {
+        console.error("Video compression error:", error);
+    }
 };
 
 
 const convertHeifToJpg = async (heifFile) => {
-    return await heic2any({
-        blob: heifFile,
-        toType: 'image/jpeg',
-        quality: 1,
-    })
-        .then((conversionResult) => conversionResult)
-        .catch((error) => {
-            console.log(error);
+    try {
+        const conversionResult = await heic2any({
+            blob: heifFile,
+            toType: "image/jpeg",
+            quality: 1,
         });
-}
+        return conversionResult;
+    } catch (error) {
+        console.error("HEIF to JPEG conversion error:", error);
+    }
+};
 
+const downloadFile = (url, filename) => {
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+};
 
+const handleCompression = async () => {
+    const inputFile = document.getElementById("file");
+    const loadingLabel = document.getElementById("loading");
+    const width = Number(document.getElementById("width")?.value) || 1920;
+    const height = Number(document.getElementById("height")?.value) || 1080;
+    const quality = Number(document.getElementById("quality")?.value) || 0.7;
+    const audioCheckbox = document.getElementById("hasAudio");
+    const hasAudio = audioCheckbox?.checked || false;
 
-const compressor = async () => {
-    const inputFile = document.getElementById('file');
-    const loadingLabel = document.getElementById('loading');
-    const width = document.getElementById('width')?.value;
-    const height = document.getElementById('height')?.value;
-    const quality = document.getElementById('quality')?.value ?? 0.7;
-    const audioCheckbox = document.getElementById('hasAudio')?.value;
-    const hasAudio = audioCheckbox === 'on' ? true : false;
+    loadingLabel.textContent = "Loading...";
 
-    const file = inputFile.files[0]
+    const file = inputFile?.files[0];
     const mimeType = file.type;
-    const fileIsAnImage = mimeType.match(/image.*/g)
+    const isImage = mimeType.startsWith("image");
 
+    try {
+        if (isImage) {
+            const imageType = mimeType.split("/")[1];
 
-    loadingLabel.replaceChildren('Loading...')
-    if (fileIsAnImage) {
-        const [imageType] = fileIsAnImage
-
-        if (imageType.includes('heif')) {
-            const jpgFile = await convertHeifToJpg(file);
-            await imageCompressor(jpgFile, width, height, quality);
+            if (imageType === "heif") {
+                const jpgFile = await convertHeifToJpg(file);
+                await imageCompressor(jpgFile, width, height, quality);
+            } else {
+                await imageCompressor(file, width, height, quality);
+            }
+        } else {
+            await videoCompressor(file, width, height, hasAudio, quality);
         }
-
-        if (!imageType.includes('heif')) {
-            console.log(file)
-            await imageCompressor(file, width, height, quality)
-        }
+    } catch (error) {
+        console.error("Compression error:", error);
     }
 
-    if (!fileIsAnImage) {
-        await videoCompressor(file, width, height, hasAudio)
-    }
-
-    loadingLabel.replaceChildren('')
-}
-
-
-
-
-
+    loadingLabel.textContent = "";
+};
